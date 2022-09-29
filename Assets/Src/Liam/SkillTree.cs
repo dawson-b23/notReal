790234ba715c7eb3efe3f1f260117afe83ed0cd8
CaptainSlow2000 <liammac352@gmail.com>
2022-09-28 23:37:15 -0700

    Added Skill Tree

---
+++ Assets/Src/Liam/SkillTree.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillTree : MonoBehaviour
+{
+    /*
+    //public PlayerData playerData;
+
+    int upgradeval = 5;
+    int requiredexp = 10;
+
+
+    void Upgrade(){
+       if(playerData.exp >= requiredexp){
+            playerData.speed += upgradeval;
+            playerData.exp -= requiredexp;
+    
+            Debug.Log("exp = " + playerData.exp);
+            Debug.Log("Upgrading");
+        }else{
+
+            Debug.Log("Not enough exp!");
+        }
+    
+    }
+    */
+}
