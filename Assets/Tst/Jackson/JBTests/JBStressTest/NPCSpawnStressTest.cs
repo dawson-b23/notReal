a8e22ee13913123b17231e682b3f71e139c60b19
Jackson Baldwin <bald2201@vandals.uidaho.edu>
2022-10-20 00:42:16 -0700

    added a few changes to stress test

--- Assets/Tst/Jackson/JBTests/JBStressTest/NPCSpawnStressTest.cs
+++ Assets/Tst/Jackson/JBTests/JBStressTest/NPCSpawnStressTest.cs
@@ -6,12 +6,13 @@ public class NPCSpawnStressTest : MonoBehaviour
 {
     private bool testActive = true;
     public GameObject NPC;
+    int cloneNum = 0;
     // Start is called before the first frame update
-    void Start()
-    {
+    //void Start()
+    //{
         //NPCRef = Resources.Load("Prefabs/Jackson/Queen BC");
         //Debug.Log("Reference loaded");
-    }
+   // }
     void Update()
     {
         while(testActive)
@@ -19,13 +20,23 @@ public class NPCSpawnStressTest : MonoBehaviour
 
             //GameObject NPC =(GameObject)Instantiate(NPCRef, NPCSpawn.position, NPCSpawn.rotation);
             Instantiate(NPC, new Vector3(0, 0, 0), Quaternion.identity);
+            WaitTest();
             Debug.Log("Created NPC Clone");
+
+            cloneNum++;
+            
         }
+    }
+
+    IEnumerator WaitTest()
+    {
+        yield return new WaitForSeconds(2);
     }
 
     void OnTriggerExit2D(Collider2D other)
      
     {
         testActive = false;
+        Debug.Log("Broke Unity at " + cloneNum);
     }
 }
