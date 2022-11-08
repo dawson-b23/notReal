32b011e110888c53089c951d5928904d0f48dab0
shreckneps <shreckneps@gmail.com>
2022-11-08 00:47:35 -0800

    Added some more weapons

--- Assets/Src/Spencer/Scripts/RegistryInitializer.cs
+++ Assets/Src/Spencer/Scripts/RegistryInitializer.cs
@@ -1,9 +1,26 @@
+/*
+ * RegistryInitializer.cs
+ * Spencer Butler
+ * A script that holds a reference to a WeaponRegistry
+ */
+
+
 using UnityEngine;
 
+
+/*
+ * Holds a reference to a WeaponRegistry
+ *
+ * Unity will not call OnEnable/Awake on ScriptableObjects without a GameObject that references them
+ * This references one, so its OnEnable method will be called
+ *
+ * member variables:
+ * registryToBeInitialized - the registry to be initialized
+ */
 public class RegistryInitializer : MonoBehaviour
 {
     [SerializeField]
-    private WeaponRegistry RegistryToBeInitialized;
+    private WeaponRegistry registryToBeInitialized;
 }
 
 
